8283576e7aedb481e823b3e6b5a319a9859a227b
miguel-apg <miguel.apg94@gmail.com>
2021-07-08 16:34:44 -0400

    Fix LocalDB

--- UI/alumnosForm.Designer.cs
+++ UI/alumnosForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace OrangeSchool.UI
+namespace OrangeSchool
 {
     partial class alumnosForm
     {
@@ -12,14 +12,6 @@ namespace OrangeSchool.UI
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
 
         #region Windows Form Designer generated code
 
--- orangeschool.sql
+++ orangeschool.sql
@@ -1,6 +1,3 @@
-DROP DATABASE IF EXISTS Orange;
-CREATE DATABASE Orange;
-
 use Orange 
 
 create table administradores(
